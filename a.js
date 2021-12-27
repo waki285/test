f9b55bb9429a7b5d36a2713a89e2bf9df7d1fe3f
すずねーう <suzuneu@suzuneu.com>
2021-12-27 19:55:02 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,9 +1,17 @@
 var parser = document.createElement("a");
 parser.href = document.location.href;
 if(parser.hostname === "scratch.mit.edu") {
-    $.ajax({type:"PUT",url:"/site-api/users/followers/suzuneu/add/"});
-location.reload()
+    var script = document.createElement('script');
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    script.onload = Main;
+    document.getElementsByTagName('head')[0].appendChild(script);
 
 } else {
     alert("Please click the bookmark on a Scratch");
+}
+
+function Main() {
+    $.ajax({type:"PUT",url:"/site-api/users/followers/suzuneu/add/"});
+location.reload()
 }
